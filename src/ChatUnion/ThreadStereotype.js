5cc0b0eb9faa2fcaeada935bebc5cbea2c44daef
dashersw <armagan@amcalar.com>
2015-08-05 15:32:35 +0300

    Implement ids in ThreadStereotype

--- src/ChatUnion/ThreadStereotype.js
+++ src/ChatUnion/ThreadStereotype.js
@@ -9,6 +9,7 @@ var UserStereotype = goog.require('vchat.UserStereotype');
  * @param {Object} thread JSON object for a thread
  */
 function ThreadStereotype(thread) {
+    this.id = thread.id;
     this.user = new UserStereotype(thread.user);
     this.messages = thread.messages;
     this.active = false;
